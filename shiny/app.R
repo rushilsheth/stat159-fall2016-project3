eb975911e6245eb793a3896e1622c594da6bffdf
rushilsheth <rsheth@berkeley.edu>
2016-11-25 11:37:32 -0800

    added if statements for both variables

--- shiny/app.R
+++ shiny/app.R
@@ -1,6 +1,6 @@
 #read in client data
 
-dat<-read.csv("../data/client_data.csv",row.names=1)
+dat<-read.csv("../data/client_data.csv")
 
 
 #UI for drop down menu
@@ -9,8 +9,8 @@ dat<-read.csv("../data/client_data.csv",row.names=1)
 UI <- pageWithSidebar(
   headerPanel('California is Artsy'),
   sidebarPanel(
-    selectInput('school', 'Schools', c("ALL", dat[,4])),
-    selectInput('facet', 'Facet', c("age", "gender", "income"))
+    selectInput('school', 'Schools', c("ALL", as.character(dat[,5]))),
+    selectInput('facet', 'Facet', c("ALL", "age", "gender", "income"))
   ),
   mainPanel(
     plotOutput(outputId = "main_plot")
@@ -23,41 +23,23 @@ server <- function(input, output) {
 output$main_plot <- renderPlot({
 
   if(input$school == "ALL"){
-    hist(dat$Avg_Fam_Inc,main="Histogram of Averag Family Income", xlab="Average Family Income",col="lightblue")
-      } else
+    if(input$facet == "ALL"){
+      hist(dat$Avg_Fam_Inc,main="Histogram of Averag Family Income", xlab="Average Family Income",col="red")
+    } else {hist(dat$Avg_Fam_Inc,main="Histogram of Averag Family Income", xlab="Average Family Income",col="green")}
+  } else{ 
+      if(input$facet == "ALL"){
+        hist(dat$Avg_Fam_Inc,main="Histogram of Averag Family Income", xlab="Average Family Income",col="yellow")
+      } else{hist(dat$Avg_Fam_Inc,main="Histogram of Averag Family Income", xlab="Average Family Income",col="blue")}
+    }
+  })
+}
 
-    hist(dat$Avg_Fam_Inc,main="Histogram of Averag Family Income", xlab="Average Family Income",col="blue")
 
-   })
 
- }
+
+
 
 
 shinyApp(ui = UI, server = server) 
 
 
-
-
-# # library(shiny) 
-# #library (pls)
-# #library (glmnet)
-
-# library(png) # For writePNG function
-
-
-# client_data <- read.csv("../data/client_data.csv")
-
-# #UI for drop down menu
-
-
-# pageWithSidebar(
-#   headerPanel('Getting Artsy in California'),
-#   sidebarPanel(
-#     selectInput('school', 'Schools', colnames(client_data)),
-#     selectInput('facet', 'Facet', c("age", "gender", "income"),
-#                 selected= "age")
-#   ),
-#   mainPanel(
-#     readPNG("../images/EDA/histogram-age.png")
-#   )
-# )
