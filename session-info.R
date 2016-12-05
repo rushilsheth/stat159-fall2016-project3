686b79562a64773dee37d8dfc5efde34d4e5ec30
rushilsheth <rsheth@berkeley.edu>
2016-12-05 14:04:29 -0800

    session: R file gives more version info

--- session-info.R
+++ session-info.R
@@ -1,5 +1,4 @@
-#add more libraries
-sink('session-info.txt', append = TRUE)
+# Loading Packages that are Used 
 library(xtable)
 library(rmarkdown)
 library(knitr)
@@ -9,5 +8,13 @@ library(pls)
 library(plyr)
 library(reshape2)
 library(ggplot2)
-sessionInfo()
+
+library(shiny)
+library(glmnet)
+
+# Generating Session-Info.txt
+sink("session-info.txt", append = TRUE)
+cat("Session Information\n\n")
+print(sessionInfo())
+devtools::session_info()
 sink()
